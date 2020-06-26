baae156e3691cd84f4c312ce94b5600461e11347
Avinash Anchal <anchalavinash66@gmail.com>
2020-06-26 14:32:26 +0530

    TouchableOpacity Alt for Button(Advance btn)

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, StyleSheet, View, Button } from 'react-native';
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
 const HomeScreen = () => {
     return <View>
@@ -7,6 +7,10 @@ const HomeScreen = () => {
         <Button 
         onPress={()=>console.log('pressed')}
         title="Go to Componets" />
+        <TouchableOpacity onPress={()=>console.log('opacity pressed')}>
+            <Text>Go to List</Text>
+            <Text>Anything here is apprad in button</Text>
+        </TouchableOpacity>
     </View>
 };
 
