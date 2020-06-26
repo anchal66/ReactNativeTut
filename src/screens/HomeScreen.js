f86c4acea1a5a02e584751a5c827856b1bf3a7a3
Avinash Anchal <anchalavinash66@gmail.com>
2020-06-26 14:27:01 +0530

    Using Button and its props

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import {Text, StyleSheet} from 'react-native';
+import { Text, StyleSheet, View, Button } from 'react-native';
 
-const HomeScreen = () =>{
-    return <Text style={styles.text}>Hi There!</Text>
+const HomeScreen = () => {
+    return <View>
+        <Text style={styles.text}>Hi There!</Text>
+        <Button 
+        onPress={()=>console.log('pressed')}
+        title="Go to Componets" />
+    </View>
 };
 
 const styles = StyleSheet.create({
-    text:{
+    text: {
         marginTop: 50,
-        fontSize:30
+        fontSize: 30
     }
 });
 
