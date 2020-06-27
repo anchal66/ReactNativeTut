a7cd66b1178d290b0a88befcdb50655e419d3715
Avinash Anchal <anchalavinash66@gmail.com>
2020-06-27 11:41:49 +0530

    Passing state as prop Passng callback func in prop

--- src/components/ColorCounter.js
+++ src/components/ColorCounter.js
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
 import { View, StyleSheet, Text, Button } from 'react-native';
 
-export default ColorCounter = () => {
+export default ColorCounter = ({color, onIncrease, onDecrease}) => {
     
     return (
         <View>
-            <Text>RED</Text>
-            <Button title="Increase Red" />
-            <Button title="Decrease Red" />
+            <Text>{color}</Text>
+            <Button
+             title={`+ ${color}`}
+             onPress={()=>onIncrease()}
+             //This just calls setRed(red+1) indirectrly
+             />
+            <Button
+             title={`- ${color}`}
+             onPress={()=>onDecrease()}
+             />
         </View>
     )
 }
--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -3,12 +3,36 @@ import { View, StyleSheet, Text, Button } from 'react-native';
 import ColorCounter from '../components/ColorCounter';
 
 export default SquareScreen = () => {
-    
+
+    const COLOR_CHANGE_VAL = 20;
+
+    const [red, setRed] = useState(0);
+    const [blue, setBlue] = useState(0);
+    const [green, setGreen] = useState(0);
+    console.log(red,blue, green);
     return (
         <View>
-            <ColorCounter />
-            <ColorCounter />
-            <ColorCounter />
+            <ColorCounter
+             onIncrease={()=>{setRed(red + COLOR_CHANGE_VAL)}}
+             onDecrease={()=>{setRed(red - COLOR_CHANGE_VAL)}}
+             color="Red"
+            />
+
+            <ColorCounter
+             onIncrease={()=>{setBlue(blue + COLOR_CHANGE_VAL)}}
+             onDecrease={()=>{setBlue(blue - COLOR_CHANGE_VAL)}}
+             color="Blue"
+            />
+            <ColorCounter
+             color="Green"
+             onIncrease={()=>{setGreen(green + COLOR_CHANGE_VAL)}}
+             onDecrease={()=>{setGreen(green - COLOR_CHANGE_VAL)}}
+            />
+            <View 
+            style={{height:150, width:150,
+             backgroundColor: `rgb(${red},${green},${blue})`
+            }} 
+            />
         </View>
     )
 }
