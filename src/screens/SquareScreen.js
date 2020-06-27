58f3a61efad7fa5ae241d77ae58bd5046d8191d8
Avinash Anchal <anchalavinash66@gmail.com>
2020-06-27 18:10:07 +0530

    Follwing convention for object declarition

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -7,45 +7,45 @@ export default SquareScreen = () => {
 
     const reducer = (state, action) => {
         //state = {red:numner, green:number, blue:number}
-        //action = {colorToChange: red|| green || blue, amount: 15 || -15}
-        switch (action.colorToChange) {
-            case 'red':
-                return state.red+ action.amount > 255 || state.red + action.amount < 0
+        //action = {type: change_red|| change_green || change_blue, payload: 15 || -15}
+        switch (action.type) {
+            case 'change_red':
+                return state.red+ action.payload > 255 || state.red + action.payload < 0
                     ? state : 
-                    {...state, red: state.red + action.amount};
+                    {...state, red: state.red + action.payload};
             //this will copy orignal state receved and override new red property
-            case 'green':
-                return state.green+ action.amount > 255 || state.green + action.amount < 0
+            case 'change_green':
+                return state.green+ action.payload > 255 || state.green + action.payload < 0
                     ? state : 
-                    {...state, green: state.green + action.amount};
-            case 'blue':
-                return state.blue+ action.amount > 255 || state.blue + action.amount < 0
+                    {...state, green: state.green + action.payload};
+            case 'change_blue':
+                return state.blue+ action.payload > 255 || state.blue + action.payload < 0
                     ? state : 
-                    {...state, blue: state.blue + action.amount};
+                    {...state, blue: state.blue + action.payload};
             default: 
                 return state;
         }
     }
     const COLOR_CHANGE_VAL = 20;
     const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 });
-
+    console.log('state: ', state)
     return (
         <View>
             <ColorCounter
-                onIncrease={() => dispatch({colorToChange: 'red', amount: COLOR_CHANGE_VAL})}
-                onDecrease = {()=> dispatch({colorToChange: 'red', amount: -1 * COLOR_CHANGE_VAL})}
+                onIncrease={() => dispatch({type: 'change_red', payload: COLOR_CHANGE_VAL})}
+                onDecrease = {()=> dispatch({type: 'change_red', payload: -1 * COLOR_CHANGE_VAL})}
                 color="Red"
             />
 
             <ColorCounter
-                onIncrease={() => dispatch({colorToChange: 'blue', amount: COLOR_CHANGE_VAL})}
-                onDecrease = {() => dispatch({colorToChange: 'blue', amount: -1 * COLOR_CHANGE_VAL})}
+                onIncrease={() => dispatch({type: 'change_blue', payload: COLOR_CHANGE_VAL})}
+                onDecrease = {() => dispatch({type: 'change_blue', payload: -1 * COLOR_CHANGE_VAL})}
              color="Blue"
             />
             <ColorCounter
                 color="Green"
-                onIncrease={() => dispatch({colorToChange: 'green', amount: COLOR_CHANGE_VAL})}
-                onDecrease={() => dispatch({colorToChange: 'green', amount: -1 * COLOR_CHANGE_VAL})}
+                onIncrease={() => dispatch({type: 'change_green', payload: COLOR_CHANGE_VAL})}
+                onDecrease={() => dispatch({type: 'change_green', payload: -1 * COLOR_CHANGE_VAL})}
             />
             <View
                 style={{
